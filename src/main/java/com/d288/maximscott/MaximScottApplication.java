1cf9e9e45ae08214ae07cfe5bffa3250fd19780a
msco659 <msco659@wgu.edu>
2025-09-29 16:52:18 -0700

    Initial Commit.

---
+++ src/main/java/com/d288/maximscott/MaximScottApplication.java
@@ -0,0 +1,13 @@
+package com.d288.maximscott;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaximScottApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaximScottApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/d288/maximscott/MaximScottApplicationTests.java
@@ -0,0 +1,13 @@
+package com.d288.maximscott;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaximScottApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
